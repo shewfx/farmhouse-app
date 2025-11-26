2db2d5bde13fdfaab8043e085bf44a8b2f0c8d59
shewfx <imshewlol@gmail.com>
2025-11-26 10:31:29 +0530

    Fix TypeScript build error in inspection page

--- app/inspection/page.tsx
+++ app/inspection/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+// Prevent build errors with useSearchParams
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { supabase } from '../utils/supabaseClient'
